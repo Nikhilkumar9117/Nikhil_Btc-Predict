01e53bed257b30fdc5c130c90e468b646f4052be
Nikhil Kumar <nikhilkumaryt4@gmail.com>
2025-05-09 09:40:00 +0530

    Update s.app.py

--- s.app.py
+++ s.app.py
@@ -1,7 +1,12 @@
+streamlit
+pandas
+numpy
+ta
+python-binance
 import streamlit as st
 import pandas as pd
 import numpy as np
-pip install ta
+import ta
 from binance.client import Client
 import datetime
 
